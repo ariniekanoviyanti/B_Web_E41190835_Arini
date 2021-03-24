71c8e2cd66bcfaaaa798075c4546a969abc8ea75
ariniekanoviyanti <ariniekanoviyanti@gmail.com>
2021-03-24 16:09:20 +0700

    Tugas Minggu 3 P3

---
+++ Pertemuan3/app/Http/Controllers/ManagementUserController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ManagementUserController extends Controller
+{
+    public function index()
+    {
+        //return "Halo ini adalah method index, dalam controller ManagementUser.";
+        //return "Method ini nantinya akan digunakan untuk mengambil semua data user";
+        $nama = "Arini Eka Noviyanti";
+
+        $pelajaran = ["Algoritma & Pemrograman", "Kalkulus", "Pemrograman Web"];
+
+        return view('home', compact('nama', 'pelajaran'));
+    }
+}
